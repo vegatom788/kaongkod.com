805cde49ae0e7a4e0f9448eaa58d6bf79d762dbf
iSPY. <vegatom788@gmail.com>
2024-07-29 22:09:18 +0800

    change

--- global/model.php
+++ global/model.php
@@ -2,11 +2,11 @@
 
 	date_default_timezone_set('Asia/Manila');
 	Class Model {
-		private $server = "127.0.0.1:3306";
-		private $username = "u510162695_kaongkod";
-		private $password = "1Kaongkod";
-		private $dbname = "u510162695_kaongkod.com";
-		private $conn;
+		public $server = "127.0.0.1:3306";
+		public $username = "u510162695_kaongkod";
+		public $password = "1Kaongkod";
+		public $dbname = "u510162695_kaongkod.com";
+		public $conn;
 
 		public function __construct() {
 			try {
@@ -14,7 +14,9 @@
 			} catch (Exception $e) {
 				echo "Connection failed" . $e->getMessage();
 			}
+			echo "connection succes";
 		}
+	}
 
 
 		
