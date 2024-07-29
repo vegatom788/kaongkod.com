6603b5f23f178f1c99e1570db5bde0f721f8ce50
iSPY. <vegatom788@gmail.com>
2024-07-30 00:22:53 +0800

    change

--- global/model.php
+++ global/model.php
@@ -2,8 +2,7 @@
 
 	date_default_timezone_set('Asia/Manila');
 	Class Model {
-		private $server = "127.0.0.1";
-		private $port = "3306";
+		private $server = "127.0.0.1:3306";
 		private $username = "u510162695_kaongkod";
 		private $password = "1Kaongkod";
 		private $dbname =  "u510162695_kaongkod";
