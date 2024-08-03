159a932e322081972980329a043fead2ec672279
Mot. <vegatom788@gmail.com>
2024-08-03 23:45:38 +0800

    change

--- admin/residents.php
+++ admin/residents.php
@@ -390,35 +390,7 @@ if (isset($_POST['archive_hidden'])) {
 														</div>
 														<div class="form-group col-2">
 															<label class="col-form-label"><b>Middlename</b></label>
-															<select class="form-control" name="mname">
-																<option value="">Select a middle name</option>
-																<option value="A.">A.</option>
-																<option value="B.">B.</option>
-																<option value="C.">C.</option>
-																<option value="D.">D.</option>
-																<option value="E.">E.</option>
-																<option value="F.">F.</option>
-																<option value="G.">G.</option>
-																<option value="H.">H.</option>
-																<option value="I.">I.</option>
-																<option value="J.">J.</option>
-																<option value="K.">K.</option>
-																<option value="L.">L.</option>
-																<option value="M.">M.</option>
-																<option value="N.">N.</option>
-																<option value="O.">O.</option>
-																<option value="P.">P.</option>
-																<option value="Q.">Q.</option>
-																<option value="R.">R.</option>
-																<option value="S.">S.</option>
-																<option value="T.">T.</option>
-																<option value="U.">U.</option>
-																<option value="V.">V.</option>
-																<option value="W.">W.</option>
-																<option value="X.">X.</option>
-																<option value="Y.">Y.</option>
-																<option value="Z.">Z.</option>
-															</select>
+															<input class="form-control" type="text" name="mname" maxlength="30">
 														</div>
 														<div class="form-group col-3">
 															<label class="col-form-label"><b>Lastname</b></label>
