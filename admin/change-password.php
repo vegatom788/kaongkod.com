610eefc1faaccf102829b26606f10cd622322ce0
Mot. <vegatom788@gmail.com>
2024-08-18 00:58:57 +0800

    change

--- admin/change-password.php
+++ admin/change-password.php
@@ -199,20 +199,20 @@
 		<script src="../dashboard/assets/js/admin.js"></script>
 		<script src='../dashboard/assets/vendors/calendar/moment.min.js'></script>	
 		<script type="text/javascript">
-			var password = document.getElementById("password"), confirm_password = document.getElementById("confirm_password");
+			var password = document.getElementById("password");
+				var confirm_password = document.getElementById("confirm_password");
 
-			function validatePassword() {
-				if(password.value != confirm_password.value) {
-					confirm_password.setCustomValidity("Passwords Don't Match");
-				} 
-
-				else {
-					confirm_password.setCustomValidity('');
+				function validatePassword() {
+					if (password.value !== confirm_password.value) {
+						confirm_password.setCustomValidity("Passwords don't match");
+					} else {
+						confirm_password.setCustomValidity('');
+					}
 				}
-			}
 
-			password.onchange = validatePassword;
-			confirm_password.onkeyup = validatePassword;
+				// Validate on input event instead of onchange or keyup
+				password.addEventListener('input', validatePassword);
+				confirm_password.addEventListener('input', validatePassword);
 		</script>
 			<script>
 					document.addEventListener('DOMContentLoaded', function() {
