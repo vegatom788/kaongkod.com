014cb74cd6caed433a199000a91b88d0b68236f1
Mot. <129591224+vegatom788@users.noreply.github.com>
2024-10-11 08:53:23 -0700

    change

--- content/footer.php
+++ content/footer.php
@@ -1,88 +1,78 @@
-        </div>
-		<!-- Page Content Box END ==== -->
-    </div>
-	<!-- Page Content Box END ==== -->
-	<!-- Footer ==== -->
-    <footer>
-        <div class="footer-top">
-			<div class="pt-exebar">
-				<div class="container">
-					<div class="d-flex align-items-stretch">
-						<div class="pt-logo mr-auto">
-							<img src="assets/images/3r.png" style="width: 285px; height: 50px;" alt=""/>
-						</div>
-						<div class="pt-social-link">
-							<ul class="list-inline m-a0">
-								<li><a href="<?php echo $fb_link; ?>" class="btn-link"><i class="fa fa-facebook"></i></a></li>
-								<!-- <li><a href="brgyvictoriareyes@gmail.com" class="btn-link"><i class="fa fa-google-plus"></i></a></li> -->
-							</ul>
-						</div>
-						<div class="pt-btn-join">
-							<a href="contact" class="btn ">Contact Us</a>
-						</div>
-					</div>
-				</div>
-			</div>
+<!-- Footer ==== -->
+<footer>
+    <div class="footer-top">
+        <div class="pt-exebar">
             <div class="container">
-                <div class="row">
-					<div class="col-lg-4">
-                        <div class="widget">
-                        	<center><img src="assets/images/k.png" style="width: 60%;"></center>
-                        </div>
+                <div class="d-flex flex-column flex-sm-row align-items-stretch">
+                    <div class="pt-logo mr-auto mb-3 mb-sm-0">
+                        <img src="assets/images/3r.png" style="width: 285px; height: 50px;" alt=""/>
                     </div>
-                    <div class="col-lg-8">
-                        <div class="row">
-							<div class="col-4">
-								<div class="widget footer_widget">
-									<h5 class="footer-title">Gov Links</h5>
-									<ul>
-										<li><a href="https://www.gov.ph/">Government Portal</a></li>
-										<li><a href="https://region7.deped.gov.ph/">Region VII Portal</a></li>
-										<li><a href="https://madridejoscebu.gov.ph/">Madridejos Gov</a></li>
-									</ul>
-								</div>
-							</div>
-							<div class="col-4">
-								<div class="widget footer_widget">
-									<h5 class="footer-title">Quick Links</h5>
-									<ul>
-										<li><a href="about-us">About Barangay Kaongkod</a></li>
-										<li><a href="officials">Brgy. Officials</a></li>
-										<li><a href="all-announcements">Barangay Announcements</a></li>
-										<li><a href="contact">Contact Us</a></li>
-									</ul>
-								</div>
-							</div>
-							<div class="col-4">
-								<div class="widget footer_widget">
-									<h5 class="footer-title">Contact Us</h5>
-									<ul>
-										<li><a href="javascript:;">Barangay Kaongkod, Madridejos, Cebu</a></li>
-										<li><a href="javascript:;">0963-129-6743</a></li>
-										<li><a href="javascript:;">barangaykaongkod15@gmail.com</a></li>
-									</ul>
-								</div>
-							</div>							
-						</div>
+                    <div class="pt-social-link mb-3 mb-sm-0">
+                        <ul class="list-inline m-a0">
+                            <li><a href="<?php echo $fb_link; ?>" class="btn-link"><i class="fa fa-facebook"></i></a></li>
+                        </ul>
                     </div>
-                    <!-- <div class="col-lg-2 col-md-12 col-sm-12 footer-col-3">
-                        <div class="widget">
-                        	<center><img src="assets/images/4.png" style="width: 170px; height: 170px;"></center>
-                        </div>
-                    </div> -->
+                    <div class="pt-btn-join">
+                        <a href="contact" class="btn">Contact Us</a>
+                    </div>
                 </div>
             </div>
         </div>
-        <div class="footer-bottom">
-            <div class="container">
-                
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4 col-md-12 mb-4">
+                    <div class="widget">
+                        <center><img src="assets/images/k.png" style="width: 60%;"></center>
+                    </div>
+                </div>
+                <div class="col-lg-8 col-md-12">
+                    <div class="row">
+                        <div class="col-6 col-md-4 mb-4">
+                            <div class="widget footer_widget">
+                                <h5 class="footer-title">Gov Links</h5>
+                                <ul>
+                                    <li><a href="https://www.gov.ph/">Government Portal</a></li>
+                                    <li><a href="https://region7.deped.gov.ph/">Region VII Portal</a></li>
+                                    <li><a href="https://madridejoscebu.gov.ph/">Madridejos Gov</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col-6 col-md-4 mb-4">
+                            <div class="widget footer_widget">
+                                <h5 class="footer-title">Quick Links</h5>
+                                <ul>
+                                    <li><a href="about-us">About Barangay Kaongkod</a></li>
+                                    <li><a href="officials">Brgy. Officials</a></li>
+                                    <li><a href="all-announcements">Barangay Announcements</a></li>
+                                    <li><a href="contact">Contact Us</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-4 mb-4">
+                            <div class="widget footer_widget">
+                                <h5 class="footer-title">Contact Us</h5>
+                                <ul>
+                                    <li><a href="javascript:;">Barangay Kaongkod, Madridejos, Cebu</a></li>
+                                    <li><a href="javascript:;">0963-129-6743</a></li>
+                                    <li><a href="mailto:barangaykaongkod15@gmail.com">barangaykaongkod15@gmail.com</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-    </footer>
-    <!-- Footer END ==== -->
-    <button class="back-to-top fa fa-chevron-up" ></button>
-    <script type="text/javascript">
-		document.addEventListener("contextmenu", function(e){
-	    e.preventDefault();
-		}, false);
-	</script>
+    </div>
+    <div class="footer-bottom">
+        <div class="container">
+            <!-- Add any additional footer content here if needed -->
+        </div>
+    </div>
+</footer>
+<!-- Footer END ==== -->
+<button class="back-to-top fa fa-chevron-up"></button>
+<script type="text/javascript">
+    document.addEventListener("contextmenu", function(e){
+        e.preventDefault();
+    }, false);
+</script>
